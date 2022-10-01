01ee180ad248f4e5f2df808c86a35ba38f89e6f4
schalkdaniel <d-schalk@t-online.de>
2022-10-01 09:34:59 +0200

    add figures

--- usecase/analyse.R
+++ usecase/analyse.R
@@ -186,42 +186,157 @@ if (FALSE) {
   ## Partial feature effects:
   ## =================================================================
 
-  fe_op_sh = sharedFEDataNum(cwb, "oldpeak")
-  gg_oldpeak_shared = ggplot(fe_op_sh, aes(x = oldpeak, y = pred)) +
-    geom_line() +
-    mytheme() +
-    ylab("Partial feature effect") +
-    ggtitle("Shared effect")
+  peDCWBNum = function(feat) {
+    fe_op_sh = sharedFEDataNum(cwb, feat)
+    ggshared = ggplot(fe_op_sh, aes_string(x = feat, y = "pred")) +
+      geom_line() +
+      mytheme() +
+      ylab("Partial feature effect") +
+      ggtitle("Shared effect")
 
-  fe_op_si = siteFEDataNum(cwb, "oldpeak")
-  gg_oldpeak_site = ggplot(fe_op_si, aes(x = oldpeak, y = pred, color = server)) +
-    geom_line() +
-    mytheme() +
-    ggsci::scale_color_jama() +
-    ylab("Partial feature effect") +
-    ggtitle("Site effects") +
-    theme(legend.title = element_blank())
+    fe_op_si = siteFEDataNum(cwb, feat)
+    ggsite = ggplot(fe_op_si, aes_string(x = feat, y = "pred", color = "server")) +
+      geom_line() +
+      mytheme() +
+      ggsci::scale_color_jama() +
+      ylab("Partial feature effect") +
+      ggtitle("Site effects") +
+      theme(legend.title = element_blank())
 
-  fe_op = fe_op_si
-  fe_op$pred = fe_op$pred + rep(fe_op_sh$pred, 4)
-  gg_oldpeak_agg = ggplot(fe_op, aes(x = oldpeak, y = pred, color = server)) +
-    geom_line() +
-    mytheme() +
-    ggsci::scale_color_jama() +
-    ylab("Partial feature effect") +
-    ggtitle("Shared + site effects") +
-    theme(legend.title = element_blank())
+    fe_op = fe_op_si
+    fe_op$pred = fe_op$pred + rep(fe_op_sh$pred, 4)
+    ggagg = ggplot(fe_op, aes_string(x = feat, y = "pred", color = "server")) +
+      geom_line() +
+      mytheme() +
+      ggsci::scale_color_jama() +
+      ylab("Partial feature effect") +
+      ggtitle("Shared + site effects") +
+      theme(legend.title = element_blank())
 
-  gg_oldpeak_site2 = gg_oldpeak_site + gg_oldpeak_agg & theme(legend.position = "bottom")
-  gg_oldpeak_site2 = gg_oldpeak_site2 + plot_layout(guides = "collect")
+    gg = ggsite + ggagg & theme(legend.position = "bottom")
+    gg = gg + plot_layout(guides = "collect")
 
-  gg_oldpeak2 = gg_oldpeak_shared + gg_oldpeak_site2 + plot_layout(widths = c(1, 2))
+    gg = ggshared + gg + plot_layout(widths = c(1, 2))
+    gg
+  }
+
+  peDCWBCat = function(feat) {
+    fe_sh = sharedFEDataCat(cwb, feat)
+    fe_sh$xnum = as.integer(as.factor(fe_sh[[feat]]))
+    fe_op = siteFEDataCat(cwb, feat)
+
+    snames = unique(fe_op$server)
+    fe_op = do.call(rbind, lapply(snames, function(s) {
+      d0 = fe_op[fe_op$server == s, ]
+      d0$predadd = d0$pred + fe_sh$pred
+      d0$pred = fe_sh$pred
+      d0$xnum = as.integer(as.factor(d0[[feat]]))
+      return(d0)
+    }))
+
+    xo = 0.3
+    fe_op$xshift = seq(-xo, xo, length.out = 5)[-3][as.integer(as.factor(fe_op$server))]
+
+    fe_sh$server = "shared"
+    ggplot() +
+      geom_hline(yintercept = 0, color = "dark gray") +
+      geom_segment(data = fe_sh, aes(x = xnum, xend = xnum, y = 0, yend = pred, color = server), size = 0.3) +
+      geom_segment(data = fe_sh, aes(x = xnum - xo, xend = xnum + xo, y = pred, yend = pred, color = server), size = 0.3) +
+      geom_segment(data = fe_op, aes(x = xnum + xshift, xend = xnum + xshift,
+        y = pred, yend = predadd, color = server), size = 0.3) +
+      geom_point(data = fe_op, aes(x = xnum + xshift, y = predadd, color = server, shape = server), show.legend = FALSE) +
+      coord_flip() +
+      xlab(feat) +
+      ylab("Coefficient") +
+      mytheme() +
+      ggsci::scale_color_jama() +
+      labs(color = "") +
+      scale_x_continuous(breaks = fe_sh$xnum, labels = fe_sh[[feat]])
+  }
+
+
+
+  gg_oldpeak2 = peDCWBNum("oldpeak")
   gg_oldpeak2
 
   if (GGSAVE) {
     ggsave(gg_oldpeak2, filename = here::here("usecase/figures/gg-oldpeak2.pdf"),
       width = 25, height = 8, units = "cm")
   }
+
+  fnums = c("age", "trestbps", "thalach", "oldpeak")
+  fcats = c("sex", "cp", "restecg", "exang")
+
+  ggs = lapply(fnums, peDCWBNum)
+  gg_nums = ggs[[1]] / ggs[[2]] / ggs[[3]] / ggs[[4]]
+
+  if (GGSAVE) {
+    ggsave(gg_nums, filename = here::here("usecase/figures/gg-pes-num.pdf"),
+      width = 25, height = 29, units = "cm")
+  }
+
+  ggs = lapply(fcats, peDCWBCat)
+  gg_cats = (ggs[[1]] + ggs[[2]]) / (ggs[[3]] + ggs[[4]]) & theme(legend.position = "bottom")
+  gg_cats = gg_cats + plot_layout(guides = "collect")
+
+  if (GGSAVE) {
+    ggsave(gg_cats, filename = here::here("usecase/figures/gg-pes-cat.pdf"),
+      width = 15, height = 17, units = "cm")
+  }
+
+
+  ## Individual contribution to prediction score:
+  ## =================================================================
+
+  newdata = data.frame(age = 60, sex = "1", cp = "4", trestbps = 90,
+    restecg = "2", oldpeak = 3, thalach = 160, exang = "1")
+  pinds = cwb$predictIndividual(newdata)
+
+
+  preds = unlist(pinds$shared)
+  dfinds = rbind(
+    data.frame(pred = unname(preds), po = 0, bl = names(preds), server = "shared"),
+    do.call(rbind, lapply(names(pinds$site), function(s) {
+      preds = unlist(pinds$site[[s]])
+      po = unlist(pinds$shared)
+      data.frame(pred = unname(preds) + po, po = po, bl = names(preds), server = s)
+    })))
+
+  dfinds$bli = as.integer(as.factor(dfinds$bl))
+  xo = 0.5
+  dfinds$xshift = seq(-xo, xo, length.out = 5)[as.integer(as.factor(dfinds$server))]
+
+  dfs = do.call(rbind, lapply(names(preds), function(pn) {
+    df0 = dfinds[dfinds$server == "shared", ]
+    i = df0$bl == pn
+    fn = strsplit(df0[i, "bl"], "-")[[1]][1]
+    flab = paste0(fn, " (", newdata[[fn]], ")")
+    data.frame(pred = df0[i, "pred"], bli = df0[i, "bli"], xshift = xo,
+      server = df0[i, "server"], feature = flab)
+  }))
+  gg_pind = ggplot(dfinds) +
+    geom_segment(data = dfs, aes(x = bli - xshift, xend = bli + xshift,
+      y = pred, yend = pred, color = server), size = 0.3) +
+    geom_segment(data = dfinds,
+      aes(x = bli + xshift, xend = bli + xshift,
+        y = po, yend = pred, color = server), size = 0.3) +
+    geom_point(data = dfinds[dfinds$server != "shared", ],
+      aes(x = bli + xshift, y = pred, color = server, shape = server), show.legend = FALSE) +
+    coord_flip() +
+    mytheme() +
+    ggsci::scale_color_jama() +
+    xlab("") +
+    ylab("Individual Contribution") +
+    scale_x_continuous(breaks = dfs$bli, labels = dfs$feature) +
+    labs(color = "")
+
+  if (GGSAVE) {
+    ggsave(gg_pind, filename = here::here("usecase/figures/gg-ind.pdf"), width = 13,
+      height = 6, units = "cm")
+  }
+  sapply(names(cwb$coef$site), function(s) cwb$predict(newdata, s))
+
+
 }
 
 
